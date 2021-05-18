4cb095c71cc9d696aaf0fa974c966c756ad5f421
psy <me@pys.im>
2021-05-19 00:50:44 +0800

    feat: 邮箱验证页面增加 loading 指示

--- pages/email/verify.vue
+++ pages/email/verify.vue
@@ -1,17 +1,22 @@
 <template>
-  <div v-if="verified" class="max-w-md mx-auto mt-16">
-    <div class="text-green-600">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-          d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-      </svg>
-    </div>
+  <div class="max-w-md mx-auto mt-16">
+    <Loading>
+      <div v-if="verified">
+        <div class="text-green-600">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+          </svg>
+        </div>
 
-    <div class="mt-8">
-      <h2 class="text-lg font-medium">
-        邮箱已验证
-      </h2>
-    </div>
+        <div class="mt-8">
+          <h2 class="text-lg font-medium">
+            邮箱已验证
+          </h2>
+        </div>
+      </div>
+    </Loading>
   </div>
 </template>
 
