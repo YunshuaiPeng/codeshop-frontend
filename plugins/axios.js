6f4e73a673272c14fbd5a3f51767fd04f270301d
psy <me@pys.im>
2021-05-19 23:14:37 +0800

    feat: 使用 axios 拦截器全局处理错误

--- plugins/axios.js
+++ plugins/axios.js
@@ -7,5 +7,20 @@ export default function({ $axios, redirect, store }) {
   })
   $axios.onError(error => {
     store.commit('loading/decrement')
+
+    const code = error.response.status
+    switch (code) {
+      case 419:
+        redirect('login')
+        break
+      case 422:
+        //
+        break
+      case 500:
+        alert(error)
+        break
+      default:
+        alert(error)
+    }
   })
 }
