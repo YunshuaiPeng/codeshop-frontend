22c163edf70d3aea8f68378c150ebf5c88c3c43b
psy <me@pys.im>
2021-05-23 13:24:50 +0800

    feat: 使用 toast 展示错误

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,4 @@
-export default function({ $axios, redirect, store }) {
+export default function({ $axios, redirect, store, $toast }) {
   $axios.onRequest(config => {
     store.commit('request/increment')
   })
@@ -10,21 +10,22 @@ export default function({ $axios, redirect, store }) {
 
     const code = error.response.status
     switch (code) {
-      case 401:
+      case 401: // 未登录
         window.location.reload()
-        redirect('login')
-        break
-      case 419:
-        redirect('login')
-        break
-      case 422:
-        //
-        break
-      case 500:
-        alert(error)
+        return redirect('login')
+      case 419: // laravel 中 csrf 验证错误
+        return redirect('login')
+      case 422: // laravel 中的参数验证错误
+        // 由页面单独处理
+        return false
+      case 405: // 请求的方法不正确，例如该用 post 却用成了 get
+      case 500: // 服务器内部错误
+        $toast.error(code + ' 发生了错误', { timeout: false })
         break
       default:
-        alert(error)
+        $toast.error(code + '发生了错误', { timeout: false })
     }
+
+    return Promise.resolve(false)
   })
 }
